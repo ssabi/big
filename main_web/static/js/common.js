2cb91c070b398f3f8a797392a13db78b83e9e743
ssabi <ensemble1224@gmail.com>
2020-03-30 11:01:42 +0900

    Update common.js

--- main_web/static/js/common.js
+++ main_web/static/js/common.js
@@ -12,28 +12,31 @@ $(document).ready(function(){
     var $rightSide = $("#right_side");
     var leftOffsetTop = $leftSide.length > 0 ? $leftSide.offset().top : 0;
     var rightOffsetTop = $rightSide.length > 0 ? $rightSide.offset().top : 0;
+    var speed = 250;
+    var defaultPos = 10;
 
     $(window).on({
         scroll : function(){
             var scrollTop = $(this).scrollTop();
             //console.log("scrollTop = "+scrollTop);
 
-            console.log("scrollTop = "+scrollTop);
-            console.log("leftOffsetTop = "+leftOffsetTop);
-
             if(scrollTop >= leftOffsetTop - 10){
                 //$leftSide.addClass("fixed");
-                $leftSide.css("top", scrollTop - leftOffsetTop + 20 + "px");
+                //$leftSide.css("top", scrollTop - leftOffsetTop + 20 + "px");
+                $leftSide.stop().animate({"top" : scrollTop - leftOffsetTop + 20}, speed);
             }else{
                 //$leftSide.removeClass("fixed");
-                $leftSide.css("top", "10px");
+                //$leftSide.css("top", "10px");
+                $leftSide.stop().animate({"top" : defaultPos}, speed);
             }
             if(scrollTop >= rightOffsetTop - 10){
                 //$rightSide.addClass("fixed");
-                $rightSide.css("top", scrollTop - rightOffsetTop + 20  + "px");
+                //$rightSide.css("top", scrollTop - rightOffsetTop + 20  + "px");
+                $rightSide.stop().animate({"top" : scrollTop - rightOffsetTop + 20}, speed);
             }else{
                 //$rightSide.removeClass("fixed");
-                $rightSide.css("top", "10px");
+                //$rightSide.css("top", "10px");
+                $rightSide.stop().animate({"top" : defaultPos}, speed);
             }
         }
     });
